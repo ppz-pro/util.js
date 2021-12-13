f8c0c1beb8515c95ee7a9a5a4be4eaee166c96a9
关于 <372301467@qq.com>
2021-12-13 17:36:45 +0800

    array.random

---
+++ array/random.js
@@ -0,0 +1,14 @@
+/** @param {Array} list */
+module.exports = function(list) {
+  const random = list.map((item, index) => ({
+    index,
+    position: Math.random()
+  })).sort(function(a, b) {
+    return a.position - b.position
+  })
+  
+  const result = []
+  for(let item of random)
+    result.push(list[item.index])
+  return result
+}
