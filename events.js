7e7ef5c5eac8e33ebd6b8c68d7680140ff56455a
关于 <372301467@qq.com>
2021-12-21 14:30:30 +0800

    event 重构

--- events.js
+++ events.js
@@ -1,32 +1,32 @@
-function on(name, handler, target) {
-  target[name] = target[name] || []
-  target[name].push(handler)
-}
-function emit(name, event, target) {
-  if(!target[name]) return
-  
-  for(let h of target[name])
-    try {
-      h(event)
-    } catch(e) {
-      console.error(`处理 ${name} 事件时，发生异常：`, e)
-    }
-}
-
 module.exports = function Events() {
-  const x = {}
-  const y = {}
-  return {
-    on(name, handler) {
-      on(name, handler, x)
-    },
-    once(name, handler) {
-      on(name, handler, y)
-    },
-    emit(name, event) {
-      emit(name, event, x)
-      emit(name, event, y)
-      y[name] = null
+  const map = {}
+  
+  function on(name, handler) {
+    map[name] = map[name] || new Set()
+    map[name].add(handler)
+  }
+  function off(name, handler) {
+    if(map[name])
+      return map[name].delete(handler)
+  }
+  function once(name, handler) {
+    function onceHandler(e) {
+      off(name, onceHandler)
+      handler(e)
     }
+    on(name, onceHandler)
+  }
+  function emit(name, event) {
+    if(!map[name]) return
+
+    for(let h of map[name])
+      try {
+        h(event)
+      } catch(e) {
+        console.error(`处理 ${name} 事件时，发生异常：`, e)
+      }
+  }
+  return {
+    on, off, once, emit
   }
 }
