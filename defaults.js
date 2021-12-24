91ce35fe871662d7c086287269f5c522c7761b74
关于 <372301467@qq.com>
2021-12-24 15:22:44 +0800

    defaultTrue

--- defaults.js
+++ defaults.js
@@ -1,7 +1,15 @@
-module.exports = function(options, d) {
+function defaults(options, d) {
   options = Object.assign({}, options) // 拷贝一份
   for(let k in d)
     if(options[k] == undefined)
       options[k] = d[k] // 因为是用 for in 取到的 k，所以 d[k] 应该是存在的
   return options
 }
+
+defaults.defaultTrue = function(target) {
+  if(target == undefined)
+    return true
+  return target
+}
+
+module.exports = defaults
